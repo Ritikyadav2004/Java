7a02bb5caa65e3565580665549abd1b820e0eb9c
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-04-08 11:36:14 +0530

    2nd Commit

---
+++ Jarvis/example.java
@@ -0,0 +1,9 @@
+public class example
+{  
+    public static void main(String[] args) {
+        int a = 130;
+        byte  b = (byte)a; 
+        System.out.println("The value of a is: " + b);
+
+    }
+}
