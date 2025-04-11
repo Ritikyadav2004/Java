3e9d8325d7e4d776b2675c77e1bd6c24223588ba
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-04-11 19:36:13 +0530

    Training_11_apr

--- checkPrime.java
+++ checkPrime.java
@@ -6,20 +6,16 @@ public class checkPrime
         Scanner s =new Scanner(System.in);
         System.out.println("Enter A Number:");
         int n = s.nextInt();
-        boolean flag = false;
-        for(int i=2;i<n/2;i++)
+        boolean flag = true;
+        for(int i=2;i<=Math.sqrt(n);i++)
         {
-            if(n%i!=0)
+            if(n% i==0)
             {
-                 flag = true;
+                 flag = false;
                  break;
             }
-            else
-            {
-                flag = false;
-                break;
-            }
-        }
+            
+       }
         if(flag)System.out.println("Prime Number: " + n);
         else System.out.println("Not Prime Number: " + n);
         
