ddcc3ac21ea28185f1cc7f852bebaa28987b8ff4
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-04-12 23:43:47 +0530

    Apr 12

--- pattern.java
+++ pattern.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class pattern 
 {
     public static void main(String[] args) {
-          Scanner s  = new Scanner(System.in);
+        System.out.println("This Code will Print Right Triangle");
+           Scanner s  = new Scanner(System.in);
             System.out.print("Enter A Number:");
             int n = s.nextInt();
         for(int i=n;i>=1;i-- )
