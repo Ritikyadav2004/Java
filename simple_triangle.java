4ad7faa9fbd3710029c550704d828db2141244bc
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-04-13 23:47:33 +0530

    Apr 13

---
+++ simple_triangle.java
@@ -0,0 +1,20 @@
+public class simple_triangle {
+    public static void main(String[] args) {
+        System.out.println("This codw will prin Normal triangle pattern.");
+        int row = 50;
+        int column = (2*row) -1;
+        for(int i=1;i<=row;i++)
+        {
+            for(int j=1;j<=(column/2)+1-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++)
+            {
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
