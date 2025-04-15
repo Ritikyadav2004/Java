714b8bd44dfbeb051af066859ec62134d614512d
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-04-15 14:08:50 +0530

    Training Apr 11

---
+++ AlphaRight.java
@@ -0,0 +1,15 @@
+public class AlphaRight {
+    public static void main(String[] args) {
+        int n=5;
+        int ch;
+        for(int i=1;i<=n;i++)
+        {   ch=64;
+            for(int j=1;j<=i;j++)
+            {    ch++;
+                System.out.print((char)ch+" ");
+            }
+            
+            System.out.println();
+        }
+    }
+}
---
+++ diomond.java
@@ -0,0 +1,35 @@
+public class diomond {
+    public static void main(String[] args) {
+        int n=10;
+
+       int i1=0;
+        for(int i=1;i<=n;i++)
+        {
+         for(int j=1;j<=n-i;j++)
+         {
+             System.out.print("   ");
+ 
+         }
+         for(int j=1;j<=2*i-1;j++)
+         {
+             System.out.print(" * ");
+         }
+         System.out.println();
+        }
+        for(int i=n;i>=1;i--)
+        {  
+            i1++;
+           for(int j=1;j<=2*i-1;j++)
+           {
+            System.out.print(" * ");
+            //System.out.print("\uD83D\uDE00");
+           }
+           System.out.println();
+           for(int k=1;k<=i1;k++)
+           {
+            System.out.print("   ");
+           }
+        }
+    }
+    
+}
