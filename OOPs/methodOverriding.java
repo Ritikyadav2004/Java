50840f223e59377eb11a2a03aa6ab177034b218c
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-05-07 13:31:31 +0530

    Training may 7

---
+++ OOPs/methodOverriding.java
@@ -0,0 +1,22 @@
+class Parents
+{
+     void eat()
+     {
+        System.out.println("Magic");
+     }
+}
+class child extends Parents
+{
+     void eat()
+     {
+        System.out.println("Jaduuu");
+     }
+}
+public class methodOverriding
+{
+     public static void main(String[] args)
+     {
+        Parents p1 = new child();
+        p1.eat();
+     }
+}
