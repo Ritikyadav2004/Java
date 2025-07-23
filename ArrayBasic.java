ad75d5a9af424cfe8905d2cc9f6f3385409463c8
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-07-23 21:15:13 +0530

    Array

--- ArrayBasic.java
+++ ArrayBasic.java
@@ -1,7 +1,7 @@
 public class ArrayBasic {
     
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 3, 4, 5 };
         for(int i=0;i<arr.length;i++)
         {
             System.out.print(i+" ");
