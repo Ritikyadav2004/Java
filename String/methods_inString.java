8d902dffa63d067bbe41f617fde21f798378a163
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-07-26 23:28:10 +0530

    Practice

--- String/methods_inString.java
+++ String/methods_inString.java
@@ -2,6 +2,7 @@ public class methods_inString
 {
     public static void main(String[] args)
     {
+        
         StringBuilder str1 = new StringBuilder("Geeks");
         System.out.println(str1.length());
         System.out.println(str1.capacity());
