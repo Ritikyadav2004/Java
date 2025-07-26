eb91f89e35b11250cf548d859ec4c95222dd1567
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-07-26 22:35:30 +0530

    StringBuilder

---
+++ String/stringbuilder.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class stringbuilder
+{
+    public static void main(String[] args)
+    {
+        // You must use the constructor to create a StringBuilder
+        StringBuilder str1 = new StringBuilder("Geeks");
+        StringBuilder str2 = str1;
+        str2.append(str1); // append modifies str2, no need to reassign
+       
+        if(str1==str2)
+        {
+            System.out.println("true");
+        }
+        else
+            System.out.println("false");
+        System.out.println(str2);
+        System.out.println(str1);
+    
+       
+    }
+}
